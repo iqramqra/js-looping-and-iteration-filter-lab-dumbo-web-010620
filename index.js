88be64b665c95d8279f08e521f787cb3f45f3b6d
Iqra Masroor <masrooriqra@gmail.com>
2020-02-17 20:45:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching (drivers, name){
+  return drivers.filter(function (driverName){
+    return driverName.toLowerCase () === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, string){
+  let lengthOfName = string.length;
+  return drivers.filter(function (driverName){
+    return driverName.slice(0, lengthOfName) === string;
+  })
+}
